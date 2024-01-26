1e67852d3d9ea21c751ae78e46d1dd10bbe25200
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-01-26 13:07:38 +0530

    updated routes

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const express = require("express");
 const app = express();
-// const { data } = require("./currencies.json");
-// const user = require('./users.json');
-const {
-  getUsersData,
-  getUserById,
-  searchUsersByQuery,
-} = require("./controllers/users.controllers");
-const {
-  getCurrenciesData,
-  getCurrenciesDataById,
-} = require("./controllers/currencies.controllers");
+
+const userRoutes = require("./routes/users.routes");
+const currencyRoutes = require("./routes/currencies.routes");
 const PORT = 8082;
 
 //Currensies Routes
-app.get("/currencies", getCurrenciesData);
-app.get("/currencies/:symbol", getCurrenciesDataById);
+app.use("/currencies", currencyRoutes);
 
 // Users Routes
 // Note: Always put static routes before dynamic routes.
-app.get("/users", getUsersData);
-app.get("/users/search", searchUsersByQuery);
-app.get("/users/:uuid", getUserById);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT 8082");
